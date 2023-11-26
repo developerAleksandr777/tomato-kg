0cd0cfa56c08aa41c1863f3602118b24619fe342
Aleksandr Likhodeev <13thjavascript@gmail.com>
2023-11-26 20:56:48 +0600

    Updates

--- src/View.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import Main from "./containers/Main/Main";
-
-const View = () => {
-  return <Routes>{<Route path="/" element={<Main />} />}</Routes>;
-};
-
-export default View;
